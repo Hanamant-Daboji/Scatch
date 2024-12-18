fc5d87591c9c712492379c14a4cad8eb271687fa
Hanamant-Daboji <sagar.tps05@gmail.com>
2024-12-18 12:27:33 +0530

    added files

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,11 @@
 const express=require("express");
 const router = express.Router();
+const ownerModel = require("../models/owner-model");
+
 
 router.get("/",(req,res)=>{
     res.send("heyyy");
 })
+console.log(process.env.NODE_ENV);
 
 module.exports=router;

341996fa4b094e2128a3d0d2a081d4dd59e507f4
Hanamant-Daboji <sagar.tps05@gmail.com>
2024-12-18 12:52:28 +0530

    Updated routes

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -7,5 +7,10 @@ router.get("/",(req,res)=>{
     res.send("heyyy");
 })
 console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV);
+
 
 module.exports=router;
